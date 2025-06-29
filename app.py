4efa18ab8c87e61132f3cafeeeb9c4387f621bf9
gabrielwanzeller <gabrielgentilwanzeller@gmail.com>
2025-06-29 17:41:45 -0300

    Revisão do chat gpt

--- app.py
+++ app.py
@@ -209,4 +209,5 @@ def handle_join(chave):
 
 if __name__ == "__main__":
     from os import environ
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+    socketio.run(app, debug=True, host="0.0.0.0",
+                 port=int(environ.get("PORT", 5000)))
